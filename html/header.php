9096389fbbcaedc37ab6b23b646968995ed9c695
matias <novillomatias2@gmail.com>
2020-01-09 15:15:21 -0300

    arreglos en el header

--- html/header.php
+++ html/header.php
@@ -1,5 +1,4 @@
-
-  <?php
+<?php
    require_once '../drivers/validaciondata.php';
     session_start();
     $mostrarRegister = "";
@@ -14,232 +13,240 @@
 
   
     ?>
-     <!-- Top Bar -->
-     <div class="top_bar">
-      <div class="container-fluid">
-          <div class="row">
-              <div class="col-xl-12 d-flex ">
-                  <div class="top_bar_contact_item ">
-                    <i class="fas fa-mobile-alt top-bar-icon"></i>
-                    +54 351 921 0101
-                  </div>
-                  <div class="top_bar_contact_item">
-                      <i class="fas fa-envelope top-bar-icon"></i>
-                      <a href="mailto:fastsales@gmail.com">Synergic@gmail.com</a>
-                  </div>
-                  <div class="top_bar_content ml-auto">
-                      <div class="top_bar_user">
-                          <div><?=$logeado?></div>
-                          <span> | </span>
-                          <div><?=$mostrarRegister?></div>
-                          <div><?=$cerrarSesion?></div>
-                      </div>
-                  </div>
-              </div>
+<!-- Top Bar -->
+<div class="top_bar">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xl-12 d-flex ">
+        <div class="top_bar_contact_item ">
+          <i class="fas fa-mobile-alt top-bar-icon"></i>
+          +54 351 921 0101
+        </div>
+        <div class="top_bar_contact_item">
+          <i class="fas fa-envelope top-bar-icon"></i>
+          <a href="mailto:fastsales@gmail.com">Synergic@gmail.com</a>
+        </div>
+        <div class="top_bar_content ml-auto">
+          <div class="top_bar_user">
+            <div><?=$logeado?></div>
+            <span> | </span>
+            <div><?=$mostrarRegister?></div>
+            <div><?=$cerrarSesion?></div>
           </div>
         </div>
       </div>
-    <!-- /Top Bar -->
-  
-  
-  <!-- header -->
-  <header>
-    <div class="container">
-      <div class="row header-row">
-        <div class="col-xl-2 col-lg-3 col-md-3 col-sm-12 col-xs-12 h-logo">
-          <a href="index.php" class="logo-img">
-            <img src="../img/logo.png" alt="">
-          </a>
-        </div>
-        <div class="col-xl-6 col-lg-8 col-md-8 col-sm-12 col-xs-12">
-          <form class="form-inline">
-            <input class="form-control mr-sm-2 inpt" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn src-btn" type="submit"><i class="fas fa-search"></i></button>
-          </form>
-        </div>
-        <div class="col-xl-4 col-lg-1 col-md-1 col-sm-12 col-xs-12 text-right text-sm-center text-xl-left">
-          <div class="user-panel">
-            <div class="up-item">
-              <div class="shopping-card">
-                <span>0</span>
-                <a href="shop cart.php">
+    </div>
+  </div>
+</div>
+<!-- /Top Bar -->
+
+
+<!-- header -->
+<header>
+  <div class="container">
+    <div class="row header-row">
+      <div class="col-xl-2 col-lg-2 col-md-12 col-sm-12 col-xs-12 h-logo">
+        <a href="index.php" class="logo-img">
+          <img src="../img/logo.png" alt="">
+        </a>
+      </div>
+      <div class="col-xl-5 col-lg-4 col-md-12 col-sm-12 col-xs-12">
+        <form class="form-inline">
+          <input class="form-control mr-sm-2 inpt" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn src-btn" type="submit"><i class="fas fa-search"></i></button>
+        </form>
+      </div>
+      <div class="col-xl-5 col-lg-6 col-md-12 col-sm-12 col-xs-12 p-0 text-center">
+        <div class="user-panel">
+          <div class="up-item">
+            <div class="shopping-card">
+              <span>0</span>
+              <a href="shop cart.php">
                 <i class="fas fa-shopping-cart"></i>
-              </a>
-            </div>
-            </div>
-          </div>
-        </div>
-      </div>
-  </header>
-  <!--/Header-->
-
-
-  <!--Nav Menu-->
-  <nav class="menu" id="menu">
-    <div class="contenedor contenedor-botones-menu">
-      <button id="btn-menu-barras" class="btn-menu-barras"><i class="fas fa-bars"></i></button>
-      <button id="btn-menu-cerrar" class="btn-menu-cerrar"><i class="fas fa-times"></i></button>
-    </div>
-
-    <div class="contenedor contenedor-enlaces-nav">
-      <div class="btn-departamentos" id="btn-departamentos">
-        <p id="cat-title">Todas las <span>Categorias</span></p>
-        <i class="fas fa-caret-down"></i>
-      </div>
-
-      <div class="enlaces">
-        <a href="index.php">Home</a>
-        <a href="contact.php">Contacto</a>
-        <a href="user-profile.php">Cuenta</a>
-        <a href="F.a.q.php">F.A.Q</a>
-      </div>
-    </div>
-
-    <div class="contenedor contenedor-grid">
-      <div class="grid" id="grid">
-        <div class="categorias">
-          <button class="btn-regresar"><i class="fas fa-arrow-left"></i> Regresar</button>
-          <h3 class="subtitulo">Categorias</h3>
-
-          <a href="productos.php" data-categoria="componentes-de-pc">Productos<i class="fas fa-angle-right"></i></a>
-          <a href="#" data-categoria="pc-de-escritorio">PC de escritorio <i class="fas fa-angle-right"></i></a>
-          <a href="#" data-categoria="monitores">Monitores <i class="fas fa-angle-right"></i></a>
-          <a href="#" data-categoria="perifericos">Perifericos <i class="fas fa-angle-right"></i></a>
-        </div>
-
-        <div class="contenedor-subcategorias">
-          <div class="subcategoria " data-categoria="componentes-de-pc">
-            <div class="enlaces-subcategoria">
-              <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
-              <h3 class="subtitulo">Productos</h3>
-              <a href="#">Procesadores</a>
-              <a href="#">Motherboards</a>
-              <a href="#">Memoria RAM</a>
-              <a href="#">Placas de video</a>
-              <a href="#">Hard Disk</a>
-            </div>
-
-            <div class="banner-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-
-            <div class="galeria-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
+                Carrito de compras
               </a>
             </div>
           </div>
-
-
-          <div class="subcategoria" data-categoria="pc-de-escritorio">
-            <div class="enlaces-subcategoria">
-              <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
-              <h3 class="subtitulo">PC de escritorio</h3>
-              <a href="#">AMD</a>
-              <a href="#">INTEL</a>
-            </div>
-
-            <div class="banner-subcategoria">
+          <div class="up-item ml-xl-5 ml-3 ml-md-5">
+            <div class="shopping-card">
               <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-
-            <div class="galeria-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-          </div>
-
-
-          <div class="subcategoria" data-categoria="monitores">
-            <div class="enlaces-subcategoria">
-              <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
-              <h3 class="subtitulo">Monitores</h3>
-              <a href="#">Acer</a>
-              <a href="#">LG</a>
-              <a href="#">Samsung</a>
-              <a href="#">Philips</a>
-            </div>
-
-            <div class="banner-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-
-            <div class="galeria-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-          </div>
-
-          <div class="subcategoria" data-categoria="perifericos">
-            <div class="enlaces-subcategoria">
-              <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
-              <h3 class="subtitulo">Perifericos</h3>
-              <a href="#">Teclados</a>
-              <a href="#">Mouses</a>
-              <a href="#">Auriculares</a>
-              <a href="#">Microfonos</a>
-              <a href="#">Webcam</a>
-            </div>
-
-            <div class="banner-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-            </div>
-
-            <div class="galeria-subcategoria">
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
-              </a>
-              <a href="#">
-                <img src="https://via.placeholder.com/300x300" alt="">
+              <i class="fas fa-heart"></i>
+              Bienvenido! usuario/a
               </a>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </nav>
-  <!--/Nav Menu-->
+</header>
+<!--/Header-->
 
+
+<!--Nav Menu-->
+<nav class="menu" id="menu">
+  <div class="contenedor contenedor-botones-menu">
+    <button id="btn-menu-barras" class="btn-menu-barras"><i class="fas fa-bars"></i></button>
+    <button id="btn-menu-cerrar" class="btn-menu-cerrar"><i class="fas fa-times"></i></button>
+  </div>
+
+  <div class="contenedor contenedor-enlaces-nav">
+    <div class="btn-departamentos" id="btn-departamentos">
+      <p id="cat-title">Todas las <span>Categorias</span></p>
+      <i class="fas fa-caret-down"></i>
+    </div>
+
+    <div class="enlaces">
+      <a href="index.php">Home</a>
+      <a href="contact.php">Contacto</a>
+      <a href="user-profile.php">Cuenta</a>
+      <a href="F.a.q.php">F.A.Q</a>
+    </div>
+  </div>
+
+  <div class="contenedor contenedor-grid">
+    <div class="grid" id="grid">
+      <div class="categorias">
+        <button class="btn-regresar"><i class="fas fa-arrow-left"></i> Regresar</button>
+        <h3 class="subtitulo">Categorias</h3>
+
+        <a href="productos.php" data-categoria="componentes-de-pc">Productos<i class="fas fa-angle-right"></i></a>
+        <a href="#" data-categoria="pc-de-escritorio">PC de escritorio <i class="fas fa-angle-right"></i></a>
+        <a href="#" data-categoria="monitores">Monitores <i class="fas fa-angle-right"></i></a>
+        <a href="#" data-categoria="perifericos">Perifericos <i class="fas fa-angle-right"></i></a>
+      </div>
+
+      <div class="contenedor-subcategorias">
+        <div class="subcategoria " data-categoria="componentes-de-pc">
+          <div class="enlaces-subcategoria">
+            <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+            <h3 class="subtitulo">Productos</h3>
+            <a href="#">Procesadores</a>
+            <a href="#">Motherboards</a>
+            <a href="#">Memoria RAM</a>
+            <a href="#">Placas de video</a>
+            <a href="#">Hard Disk</a>
+          </div>
+
+          <div class="banner-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+
+          <div class="galeria-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+        </div>
+
+
+        <div class="subcategoria" data-categoria="pc-de-escritorio">
+          <div class="enlaces-subcategoria">
+            <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+            <h3 class="subtitulo">PC de escritorio</h3>
+            <a href="#">AMD</a>
+            <a href="#">INTEL</a>
+          </div>
+
+          <div class="banner-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+
+          <div class="galeria-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+        </div>
+
+
+        <div class="subcategoria" data-categoria="monitores">
+          <div class="enlaces-subcategoria">
+            <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+            <h3 class="subtitulo">Monitores</h3>
+            <a href="#">Acer</a>
+            <a href="#">LG</a>
+            <a href="#">Samsung</a>
+            <a href="#">Philips</a>
+          </div>
+
+          <div class="banner-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+
+          <div class="galeria-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+        </div>
+
+        <div class="subcategoria" data-categoria="perifericos">
+          <div class="enlaces-subcategoria">
+            <button class="btn-regresar"><i class="fas fa-arrow-left"></i>Regresar</button>
+            <h3 class="subtitulo">Perifericos</h3>
+            <a href="#">Teclados</a>
+            <a href="#">Mouses</a>
+            <a href="#">Auriculares</a>
+            <a href="#">Microfonos</a>
+            <a href="#">Webcam</a>
+          </div>
+
+          <div class="banner-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+
+          <div class="galeria-subcategoria">
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+            <a href="#">
+              <img src="https://via.placeholder.com/300x300" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+<!--/Nav Menu-->
