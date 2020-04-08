24fb57812b61b90868b48bbd6fca9e4ca2b3a195
matias <novillomatias2@gmail.com>
2020-04-08 18:01:38 -0300

    Cart

--- app/Http/Controllers/CarritoController.php
+++ app/Http/Controllers/CarritoController.php
@@ -4,9 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Categoria;
+use App\Producto;
 class CarritoController extends Controller
 {
     public function view(){
-        return view('carrito');
+        $cartItems = \Cart::session(auth()->id())->getContent();
+        return view('carrito',compact('cartItems'));
+    }
+
+    public function add($product){
+
+        $product = Producto::find($product);
+        $foto = $product->fotos()->first();
+        $imagen = $foto->nombre;
+       // add the product to cart
+        \Cart::session(auth()->id())->add(array(
+            'id' => $product->id,
+            'name' => $product->nombre,
+            'price' => $product->precio,
+            'quantity' => 1,
+            'attributes' => array(
+                "imagen" => $imagen
+            ),
+            'associatedModel' => $product
+        ));
+
+        return redirect()->route('cart');
+    }
+
+    public function destroy($itemId){
+        \Cart::session(auth()->id())->remove($itemId);
+        return back();
+    }
+
+    public function update($itemId){
+        \Cart::session(auth()->id())->update($itemId, array(
+            'quantity' => array(
+                'relative' => false,
+                'value' => request('quantity')
+            )
+        ));
+        return back();
+    }
+
+    public function clear(){
+        \Cart::session(auth()->id())->clear();
+        return back();
     }
 }
--- resources/views/carrito.blade.php
+++ resources/views/carrito.blade.php
@@ -3,110 +3,85 @@
 <div class="cart_section">
     <div class="container">
         <div class="row">
+       
             <div class="col-lg-10 offset-lg-1">
+                @if(Cart::session(auth()->id())->getContent()->count() > 0)
                 <div class="cart_container">
                     <div class="cart_title">Carrito de Compras</div>
+                    @foreach ($cartItems as $item )
+                    
                     <div class="cart_items">
                         <ul class="cart_list p-0">
                             <li class="cart_item clearfix">
-                                <div class="cart_item_image"><img src="../img/pc.png" alt=""></div>
+                            <div class="cart_item_image"><img src="/storage/{{$item->attributes->imagen}}" alt=""></div>
                                 <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
                                     <div class="cart_item_name cart_info_col">
                                         <div class="cart_item_title">Producto</div>
                                         <div class="cart_item_text">
-                                        <a href="">
+                                        <a href="{{route('productoDetail', str_replace(" ", "+", $item->name))}}">
                                         <p>
-                                                Pc AMD Gamer ZEN Kronos
-                                                Ryzen 5 3400G
-                                                Ssd 240Gb 8GB (2x4gb) |
+                                                {{$item->name}}
                                         </p>
                                         </a>
                                         </div>
                                     </div>
 
                                     <div class="cart_item_quantity cart_info_col">
-                                        <div class="cart_item_title">Cantidad</div>
+                                        <div class="cart_item_title text-center">Cantidad</div>
                                         <div class="cart_item_text mt-lg-4">
-                                        <form action="">
-                                        <div class="form-group">
-                                            <input type="number" value="1" class="form-control inp-cant">
+                                        {{-- Modificar cantidad --}}
+                                        <form action="{{route('cart.update',$item->id)}}">
+                                        <div class="form-group d-flex">
+                                            <input type="number" name="quantity" value="{{$item->quantity}}" class="form-control inp-cant">
+                                            <input type="submit" class="btn btn-small btn-sm btn-info mx-1 " value="actualizar">
                                         </div>
                                         </form>
-                                        </div>
-                                    </div>
-                                    <div class="cart_item_price cart_info_col">
-                                        <div class="cart_item_title">Precio</div>
-                                        <div class="cart_item_text mt-lg-4">$2000</div>
-                                    </div>
-                                    <div class="cart_item_total cart_info_col">
-                                        <div class="cart_item_title">Sub Total</div>
-                                        <div class="cart_item_text mt-lg-4">$2000</div>
-                                    </div>
-                                    <div class="cart_item_total cart_info_col">
-                                        <div class="cart_item_title">Quitar del carrito</div>
-                                        <div class="cart_item_text mt-lg-4 text-center">
-                                          <button><i class="fas fa-cart-arrow-down h4 cart-down"></i></button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="cart_item clearfix">
-                                <div class="cart_item_image"><img src="../img/intelpc.jpg" alt=""></div>
-                                <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
-                                    <div class="cart_item_name cart_info_col">
-                                        <div class="cart_item_title">Producto</div>
-                                        <div class="cart_item_text">
-                                            <p>
-                                            <a href="#">
-                                            PC INTEL Desing PRO CORE I7 9700 Nvidia Geforce Rtx 2060
-                                            </a>
-                                            </p>
-                                        </div>
-                                    </div>
 
-                                    <div class="cart_item_quantity cart_info_col">
-                                        <div class="cart_item_title">Cantidad</div>
-                                        <div class="cart_item_text mt-lg-4">
-                                        <form action="">
-                                        <div class="form-group">
-                                            <input type="number" value="1" class="form-control inp-cant">
-                                        </div>
-                                        </form>
                                         </div>
                                     </div>
                                     <div class="cart_item_price cart_info_col">
-                                        <div class="cart_item_title">Precio</div>
-                                        <div class="cart_item_text mt-lg-4">$2000</div>
-                                    </div>
+                                        <div class="cart_item_title text-center">Precio</div>
+                                            <div class="cart_item_text mt-lg-4">${{number_format($item->price,'2')}}</div>
+                                        </div>
                                     <div class="cart_item_total cart_info_col">
-                                        <div class="cart_item_title">Sub Total</div>
-                                        <div class="cart_item_text mt-lg-4">$2000</div>
+                                        <div class="cart_item_title text-center">Sub Total</div>
+                                        <div class="cart_item_text mt-lg-4">${{number_format(Cart::session(auth()->id())->get($item->id)->getPriceSum(),'2')}}</div>
                                     </div>
                                     <div class="cart_item_total cart_info_col">
                                         <div class="cart_item_title">Quitar del carrito</div>
                                         <div class="cart_item_text mt-lg-4 text-center">
-                                         <button><i class="fas fa-cart-arrow-down h4 cart-down"></i></button>
+                                        <a href="{{route('cart.destroy', $item->id)}}" class="cart-down"><i class="fas fa-cart-arrow-down h4 cart-down"></i></a>
                                         </div>
                                     </div>
                                 </div>
                             </li>
+                           
 
                         </ul>
                     </div>
+                    @endforeach
+                    
                     <!-- ----------------------------------------------------------------------- -->
 
                     <!-- Order Total -->
                     <div class="order_total">
                         <div class="order_total_content text-md-right col-12">
                             <div class="order_total_title">Total: </div>
-                            <div class="order_total_amount">$2000</div>
+                            <div class="order_total_amount">${{number_format(Cart::session(auth()->id())->getTotal(),'2')}}</div>
                         </div>
 
                     </div>
                     <div class="cart_buttons mt-sm-auto mt-lg-4 mt-md-4 text-md-right">
-                        <button type="button" class="button cart_button_clear">Vaciar Carrito</button>
-                        <button type="button" class="button cart_button_checkout">Finalizar compra</button>
+                        <form action="{{route('cart.clear')}}" method="GET">
+                            <button type="submit" class="button cart_button_clear">Vaciar Carrito</button>
+                        </form>
                     </div>
+
+                    @else
+                        <div class="container text-muted text-center">
+                            <h4>No hay productos en el carrito! <a href="/productos">Encontra lo que buscas aqui!</a></h4>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
