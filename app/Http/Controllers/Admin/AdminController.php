e931a2ed250ce4e9ac1f0bed71da1b2c2872c78d
matias <novillomatias2@gmail.com>
2020-04-23 20:17:14 -0300

    fix admin stock

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -16,7 +16,8 @@ class AdminController extends Controller
     public function index(){
         $dia= date("y-m-d");
         $newusers = User::where('created_at', $dia)->get();
-        $productos = Producto::where('stock','>',0)->get();
+        $productos = Producto::where('stock','<=',0)->get();
+        
         $sinstock = count($productos);
         
         return view('admin.admin',compact('newusers','sinstock'));
